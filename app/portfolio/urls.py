e650e1c06a83ba574dfeb1063c0d034775712c5d
amanuel g/silassie <amanulee2@gmail.com>
2023-01-15 15:35:34 +0000

    ...

--- app/portfolio/urls.py
+++ app/portfolio/urls.py
@@ -21,7 +21,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='portfolio-home'),
-    path('a/', views.a)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'views.error_404_view'
