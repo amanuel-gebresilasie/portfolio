319edb9225846bf43b87bd78fef7f7b9c74f118f
amanuel g/silassie <amanulee2@gmail.com>
2023-01-15 15:18:20 +0000

    ...

--- app/portfolio/urls.py
+++ app/portfolio/urls.py
@@ -22,4 +22,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='portfolio-home'),
     path('a/', views.a)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
