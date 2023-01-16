2a90df8a2197a0554b005044960ea83fa3ab0a48
amanuel g/silassie <amanulee2@gmail.com>
2023-01-16 04:36:27 +0000

    ...

--- app/portfolio/models.py
+++ app/portfolio/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.conf import settings
 
 class Projects(models.Model):
-    img = models.ImageField()
+    img = models.ImageField(upload_to="static/portfolio/media")
     view_link = models.TextField()
     github_link = models.TextField()
